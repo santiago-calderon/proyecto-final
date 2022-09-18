32910565bcada808af5d3942525eed45021b674c
Santiago Calderón <santi.calderon.sc@gmail.com>
2022-09-17 21:18:29 -0300

    agregando gracias

--- pages/enviar2.php
+++ pages/enviar2.php
@@ -12,13 +12,13 @@ $header .= "Content-Type: text/plain";
 $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
 $mensaje .= "Su e-mail es: " . $mail . " \r\n";
 $mensaje .= "Su telefono es: " . $phone . " \r\n";
-$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['message'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'estancialasmarias@gmail.com';
+$para = 'glampinglasmarias@gmail.com';
 $asunto = $_POST['affair'];
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
 
-header("Location:index.html");
+header("Location:gracias.html");
 ?>
